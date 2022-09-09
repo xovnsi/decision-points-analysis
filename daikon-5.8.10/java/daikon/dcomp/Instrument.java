cdb2db5866f2b8cd403ecb4672d0f02e3bc44274
Diego Savoia <diego.savoia@mail.polimi.it>
2022-09-09 16:16:24 +0200

    Uploaded Daikon for Streamlit deployment

---
+++ daikon-5.8.10/java/daikon/dcomp/Instrument.java
@@ -0,0 +1,247 @@
+package daikon.dcomp;
+
+import daikon.DynComp;
+import daikon.plumelib.bcelutil.BcelUtil;
+import daikon.plumelib.bcelutil.SimpleLog;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.IllegalClassFormatException;
+import java.security.ProtectionDomain;
+import org.apache.bcel.*;
+import org.apache.bcel.classfile.*;
+import org.apache.bcel.generic.*;
+import org.checkerframework.checker.nullness.qual.Nullable;
+import org.checkerframework.checker.signature.qual.InternalForm;
+import org.checkerframework.dataflow.qual.Pure;
+
+public class Instrument implements ClassFileTransformer {
+
+  /** Directory for debug output. */
+  File debug_dir;
+  /** Directory for debug instrumented class output. */
+  File debug_bin_dir;
+  /** Directory for debug original class output. */
+  File debug_orig_dir;
+  /** Have we seen a class member of a known transformer? */
+  static boolean transformer_seen = false;
+
+  /** Debug information about which classes are transformed and why. */
+  private static SimpleLog debug_transform = new SimpleLog(false);
+
+  /** Instrument class constructor. Setup debug directories, if needed. */
+  public Instrument() {
+    debug_transform.enabled =
+        DynComp.debug || DynComp.debug_transform || Premain.debug_dcinstrument;
+    daikon.chicory.Instrument.debug_transform.enabled = debug_transform.enabled;
+
+    debug_dir = DynComp.debug_dir;
+    debug_bin_dir = new File(debug_dir, "bin");
+    debug_orig_dir = new File(debug_dir, "orig");
+
+    if (DynComp.dump) {
+      debug_bin_dir.mkdirs();
+      debug_orig_dir.mkdirs();
+    }
+  }
+
+  /** Debug code for printing the current run-time call stack. */
+  public static void print_call_stack() {
+    StackTraceElement[] stack_trace;
+    stack_trace = Thread.currentThread().getStackTrace();
+    // [0] is getStackTrace
+    // [1] is print_call_stack
+    for (int i = 2; i < stack_trace.length; i++) {
+      System.out.printf("call stack: %s%n", stack_trace[i]);
+    }
+    System.out.println();
+  }
+
+  @Override
+  @SuppressWarnings("nullness") // bug: java.lang.instrument is not yet annotated
+  public byte @Nullable [] transform(
+      ClassLoader loader,
+      @InternalForm String className,
+      Class<?> classBeingRedefined,
+      ProtectionDomain protectionDomain,
+      byte[] classfileBuffer)
+      throws IllegalClassFormatException {
+
+    debug_transform.log(
+        "In dcomp.Instrument.transform(): class = %s, loader: %s%n", className, loader);
+
+    if (className == null) {
+      /*
+      // debug code to display unnamed class
+      try {
+        // Parse the bytes of the classfile, die on any errors
+        ClassParser parser = new ClassParser(new ByteArrayInputStream(classfileBuffer), className);
+        JavaClass c = parser.parse();
+        System.out.println(c.toString());
+      } catch (Throwable e) {
+        System.out.printf("Unexpected Error: %s%n", e);
+        e.printStackTrace();
+        throw new RuntimeException("Unexpected error", e);
+      }
+      */
+      // most likely a lambda related class
+      return null;
+    }
+
+    // See comments in Premain.java about meaning and use of in_shutdown.
+    if (Premain.in_shutdown) {
+      debug_transform.log("Skipping in_shutdown class %s%n", className);
+      return null;
+    }
+
+    // If already instrumented, nothing to do
+    // (This set will be empty if DCInstrument.jdk_instrumented is false)
+    if (Premain.pre_instrumented.contains(className)) {
+      debug_transform.log("Skipping pre_instrumented JDK class %s%n", className);
+      return null;
+    }
+
+    boolean in_jdk = false;
+
+    // Check if class is in JDK
+    if (BcelUtil.inJdkInternalform(className)) {
+      // If we are not using an instrumented JDK, then skip this class.
+      if (!DCInstrument.jdk_instrumented) {
+        debug_transform.log("Skipping JDK class %s%n", className);
+        return null;
+      }
+
+      int lastSlashPos = className.lastIndexOf('/');
+      if (lastSlashPos > 0) {
+        String packageName = className.substring(0, lastSlashPos).replace('/', '.');
+        if (Premain.problem_packages.contains(packageName)) {
+          debug_transform.log("Skipping problem package %s%n", packageName);
+          return null;
+        }
+      }
+
+      if (BcelUtil.javaVersion > 8) {
+        if (Premain.problem_classes.contains(className.replace('/', '.'))) {
+          debug_transform.log("Skipping problem class %s%n", className);
+          return null;
+        }
+      }
+
+      if (className.equals("java/lang/DCRuntime")) {
+        debug_transform.log("Skipping special DynComp runtime class %s%n", className);
+        return null;
+      }
+
+      in_jdk = true;
+      debug_transform.log("Instrumenting JDK class %s%n", className);
+    } else {
+
+      // We're not in a JDK class
+      // Don't instrument our own classes
+      if (is_dcomp(className)) {
+        debug_transform.log("Skipping is_dcomp class %s%n", className);
+        return null;
+      }
+
+      // Don't instrument other byte code transformers
+      if (is_transformer(className)) {
+        debug_transform.log("Skipping is_transformer class %s%n", className);
+        if (!transformer_seen) {
+          transformer_seen = true;
+          System.err.printf(
+              "DynComp warning: This program uses a Java byte code transformer: %s%n", className);
+          System.err.printf(
+              "This may interfere with the DynComp transformer and cause DynComp to fail.%n");
+        }
+        return null;
+      }
+    }
+
+    if (loader == null) {
+      debug_transform.log("transforming class %s, loader %s%n", className, loader);
+    } else {
+      debug_transform.log(
+          "transforming class %s, loader %s - %s%n", className, loader, loader.getParent());
+    }
+
+    try {
+      // Parse the bytes of the classfile, die on any errors
+      ClassParser parser = new ClassParser(new ByteArrayInputStream(classfileBuffer), className);
+      JavaClass c = parser.parse();
+
+      if (DynComp.dump) {
+        c.dump(new File(debug_orig_dir, c.getClassName() + ".class"));
+      }
+
+      // Transform the file
+      DCInstrument dci = new DCInstrument(c, in_jdk, loader);
+      JavaClass njc = dci.instrument();
+
+      if (njc == null) {
+        debug_transform.log("Didn't instrument %s%n", c.getClassName());
+        return null;
+      } else {
+        if (DynComp.dump) {
+          System.out.printf("Dumping %s to %s%n", njc.getClassName(), debug_bin_dir);
+          // write .class file
+          njc.dump(new File(debug_bin_dir, njc.getClassName() + ".class"));
+          // write .bcel file
+          BcelUtil.dump(njc, debug_bin_dir);
+        }
+        return (njc.getBytes());
+      }
+    } catch (Throwable e) {
+      System.err.printf("Unexpected Error: %s%n", e);
+      e.printStackTrace();
+      throw new RuntimeException("Unexpected error", e);
+    }
+  }
+
+  /**
+   * Returns whether or not the specified class is part of dcomp itself (and thus should not be
+   * instrumented). Some Daikon classes that are used by DynComp are included here as well.
+   *
+   * @param classname class to be checked
+   * @return true if classname is a part of DynComp
+   */
+  @Pure
+  private static boolean is_dcomp(String classname) {
+
+    if (classname.startsWith("daikon/dcomp/") && !classname.startsWith("daikon/dcomp/DcompTest")) {
+      return true;
+    }
+    if (classname.startsWith("daikon/chicory/")
+        && !classname.equals("daikon/chicory/ChicoryTest")) {
+      return true;
+    }
+    if (classname.equals("daikon/PptTopLevel$PptType")) {
+      return true;
+    }
+    if (classname.startsWith("daikon/plumelib")) {
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * Returns whether or not the specified class is part of a tool known to do Java byte code
+   * transformation. We need to warn user this may not work correctly.
+   *
+   * @param classname class to be checked
+   * @return true if classname is a known transformer
+   */
+  @Pure
+  protected static boolean is_transformer(String classname) {
+
+    if (classname.startsWith("org/mockito")) {
+      return true;
+    }
+    if (classname.startsWith("org/objenesis")) {
+      return true;
+    }
+    if (classname.contains("ByMockito")) {
+      return true;
+    }
+    return false;
+  }
+}
---
+++ daikon-5.8.10/java/daikon/dcomp/Premain.java
@@ -0,0 +1,375 @@
+package daikon.dcomp;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import daikon.DynComp;
+import daikon.chicory.DaikonVariableInfo;
+import daikon.chicory.DeclWriter;
+import daikon.plumelib.bcelutil.BcelUtil;
+import daikon.plumelib.options.Option;
+import daikon.plumelib.options.Options;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.lang.instrument.ClassDefinition;
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.Instrumentation;
+import java.net.URL;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import org.apache.bcel.*;
+import org.apache.bcel.classfile.*;
+import org.apache.bcel.generic.*;
+
+/**
+ * This class is the entry point for the DynComp instrumentation agent. It is the only code in
+ * dcomp_premain.jar.
+ */
+public class Premain {
+
+  // These command-line options cannot be accessed from DynComp.  These are internal debugging
+  // options that may be used when Premain is invoked directly from the command line.
+
+  /** Turn on basic DCInstrument debugging options. */
+  @Option("Turn on basic DCInstrument debugging options")
+  public static boolean debug_dcinstrument = false;
+
+  /** Turn on basic DCRuntime debugging options. */
+  @Option("Turn on basic DCRuntime debugging options")
+  public static boolean debug_dcruntime = false;
+
+  /** Turn on most DCRuntime debugging options. */
+  @Option("Turn on most DCRuntime debugging options")
+  public static boolean debug_dcruntime_all = false;
+
+  /** If true, print information about the classes being transformed. */
+  public static boolean verbose = false;
+
+  /** Set of pre-instrumented JDK classes. */
+  protected static Set<String> pre_instrumented = new HashSet<>();
+
+  /** Set of packages known to cause problems when instrumented. */
+  protected static Set<String> problem_packages =
+      new HashSet<>(
+          Arrays.asList(
+              // Packages to support reflection and Lambda expressions cause instrumentation
+              // problems and probably don't affect user program comparability values.
+              // JDK8 and JDK11
+              "java.lang.invoke",
+              "java.lang.reflect",
+              "sun.reflect.annotation",
+              "sun.reflect.misc",
+              // JDK8
+              "sun.reflect",
+              // JDK11
+              "jdk.internal.reflect"));
+
+  /** Set of classes known to cause problems when instrumented. */
+  protected static Set<String> problem_classes =
+      new HashSet<>(
+          Arrays.asList(
+              // (none at present)
+              ));
+
+  /** Set of methods known to cause problems when instrumented. */
+  protected static Set<String> problem_methods =
+      new HashSet<>(
+          Arrays.asList(
+              // (none at present)
+              ));
+
+  /**
+   * One of the last phases for DynComp is to write out the comparability values after the user
+   * program completes execution. One of the steps is to assign values to the arguments of methods
+   * that have not been executed. We use reflection to get type information about these arguments,
+   * which causes the method to be loaded; which causes the main part of DynComp to try to
+   * instrument the method. As the user program has completed execution, doing instrumentation at
+   * this point can lead to problems. The correct fix for this problem is to use BCEL to get the
+   * type information instead of reflection, thus avoiding loading the method into the JVM. This
+   * will be a large change, so a temporary fix is to indicate if the program is in shutdown mode
+   * and not instrument any methods when this flag is true. TODO: Couldn't we just call
+   * removeTransformer at the start of shutdown?
+   */
+  protected static boolean in_shutdown = false;
+
+  // For debugging
+  // protected static Instrumentation instr;
+
+  /**
+   * This method is the entry point of the Java agent. Its main purpose is to set up the transformer
+   * so that when classes from the target app are loaded, they are first transformed in order to add
+   * comparability instrumentation.
+   *
+   * @param agentArgs string containing the arguments passed to this agent
+   * @param inst instrumentation instance to be used to transform classes
+   * @throws IOException if jdk_classes.txt cannot be read or if the correct version of BCEL cannot
+   *     be found or loaded
+   */
+  public static void premain(String agentArgs, Instrumentation inst) throws IOException {
+    // For debugging
+    // this.instr = inst;
+
+    // Because DynComp started Premain in a separate process, we must rescan
+    // the options to set up the DynComp static variables.
+    Options options = new Options(DynComp.synopsis, DynComp.class, Premain.class);
+    String[] target_args = options.parse(true, agentArgs.split("  *"));
+    if (target_args.length > 0) {
+      System.err.printf("Unexpected Premain arguments %s%n", Arrays.toString(target_args));
+      options.printUsage();
+      System.exit(1);
+    }
+
+    verbose = DynComp.verbose || DynComp.debug;
+
+    if (DynComp.rt_file != null && DynComp.rt_file.getName().equalsIgnoreCase("NONE")) {
+      DynComp.no_jdk = true;
+      DynComp.rt_file = null;
+    }
+
+    // Note that the following references to static fields have an important
+    // side effect: They cause the corresponding class to be loaded. This helps
+    // Instrument.transform() avoid ClassCircularityErrors during initialization.
+    DaikonVariableInfo.std_visibility = DynComp.std_visibility;
+    DCRuntime.depth = DynComp.nesting_depth;
+    // daikon.chicory.Instrument#shouldIgnore is shared by Chicory and DynComp.
+    // It uses the Chicory Runtime copy of the patterns.
+    daikon.chicory.Runtime.ppt_omit_pattern = DynComp.ppt_omit_pattern;
+    daikon.chicory.Runtime.ppt_select_pattern = DynComp.ppt_select_pattern;
+
+    DCInstrument.jdk_instrumented = !DynComp.no_jdk;
+    @SuppressWarnings("UnusedVariable") // loads the BcelUtil class; otherwise, Premain gives errors
+    int junk = BcelUtil.javaVersion;
+
+    // Another 'trick' to force needed classes to be loaded prior to retransformation.
+    String buffer =
+        String.format(
+            "In DynComp premain, agentargs ='%s', Instrumentation = '%s'", agentArgs, inst);
+    if (verbose) {
+      System.out.println(buffer);
+      System.out.printf("Options settings: %n%s%n", options.settings());
+    }
+
+    // Read the list of pre-instrumented classes.
+    if (DCInstrument.jdk_instrumented) {
+      // location is: daikon/java/dcomp-rt/java/lang/jdk_classes.txt .
+      InputStream strm = Object.class.getResourceAsStream("jdk_classes.txt");
+      if (strm == null) {
+        System.err.println(
+            "Can't find jdk_classes.txt;"
+                + " see Daikon manual, section \"Instrumenting the JDK with DynComp\"");
+        System.exit(1);
+      }
+      BufferedReader reader = new BufferedReader(new InputStreamReader(strm, UTF_8));
+
+      while (true) {
+        String line = reader.readLine();
+        if (line == null) {
+          break;
+        }
+        // System.out.printf("adding '%s'%n", line);
+        pre_instrumented.add(line);
+      }
+    }
+
+    // Setup the shutdown hook
+    Thread shutdown_thread = new ShutdownThread();
+    java.lang.Runtime.getRuntime().addShutdownHook(shutdown_thread);
+
+    // Setup the transformer
+    ClassFileTransformer transformer;
+    // use a special classloader to ensure correct version of BCEL is used
+    ClassLoader loader = new daikon.chicory.ChicoryPremain.ChicoryLoader();
+    try {
+      transformer =
+          (ClassFileTransformer)
+              loader.loadClass("daikon.dcomp.Instrument").getDeclaredConstructor().newInstance();
+    } catch (Exception e) {
+      throw new RuntimeException("Unexpected error loading Instrument", e);
+    }
+    if (verbose) {
+      // If DCInstrument.jdk_instrumented is true then the printf below will output
+      // 'null' to indicate we are using the bootstrap loader.
+      System.out.printf(
+          "Classloader of transformer = %s%n", transformer.getClass().getClassLoader());
+    }
+
+    // Check that we got a newer version of BCEL that includes JDK 11 support. At present,
+    // this is only the PLSE 6.4.1.1 release version.  We can verify this version by the
+    // presence of the method FieldGenOrMethodGen.removeAnnotationEntries().
+    try {
+      Class<?> c = loader.loadClass("org.apache.bcel.generic.FieldGenOrMethodGen");
+      c.getMethod("removeAnnotationEntries", (Class<?>[]) null);
+    } catch (Exception e) {
+      System.err.printf("%nBCEL jar found is not the version included with the Daikon release.%n");
+      System.exit(1);
+    }
+
+    // now turn on instrumentation
+    if (verbose) {
+      System.out.println("call addTransformer");
+    }
+    inst.addTransformer(transformer, true);
+
+    // See the "General Java Runtime instrumentation strategy" comments in DCInstrument.java
+    // for an explaination of how we deal with instrumenting the JDK 11 runtime.
+    //
+    // At this point in DynComp start up, we use java.lang.instrument.redefineClasses to replace the
+    // dummy java.lang.DCRuntime with a version where each method calls the corresponding method in
+    // daikon.dcomp.DCRuntime. The Java runtime does not enforce the security check in this case.
+    //
+    if (BcelUtil.javaVersion > 8 && DCInstrument.jdk_instrumented) {
+
+      // Buffer for input of our replacement java.lang.DCRuntime.
+      // The size of the current version is 6326 bytes and we do not
+      // anticipate any significant changes.
+      byte[] repClass = new byte[9999];
+      String classname = "daikon/dcomp-transfer/DCRuntime.class";
+      URL class_url = ClassLoader.getSystemResource(classname);
+      if (class_url != null) {
+        try {
+          InputStream inputStream = class_url.openStream();
+          if (inputStream != null) {
+            int size = inputStream.read(repClass, 0, repClass.length);
+            byte[] truncated = new byte[size];
+            System.arraycopy(repClass, 0, truncated, 0, size);
+            ClassDefinition cd =
+                new ClassDefinition(Class.forName("java.lang.DCRuntime"), truncated);
+            inst.redefineClasses(cd);
+          } else {
+            throw new Error("openStream failed for " + class_url);
+          }
+        } catch (Throwable t) {
+          throw new Error("Unexpected error reading " + class_url, t);
+        }
+      } else {
+        throw new Error("Could not locate " + classname);
+      }
+    }
+
+    // Initialize the static tag array
+    if (verbose) {
+      System.out.println("call DCRuntime.init");
+    }
+    DCRuntime.init();
+
+    if (verbose) {
+      System.out.println("exit premain");
+    }
+  }
+
+  /** Shutdown thread that writes out the comparability results. */
+  public static class ShutdownThread extends Thread {
+
+    @Override
+    public void run() {
+
+      if (Premain.verbose) {
+        System.out.println("in shutdown");
+      }
+      in_shutdown = true;
+
+      // for debugging
+      // Class<?>[] loaded_classes = instr.getAllLoadedClasses();
+      // for (Class<?> loaded_class : loaded_classes) {
+      //   System.out.println(loaded_class + ": " + loaded_class.getClassLoader());
+      // }
+
+      // If requested, write the comparability data to a file
+      if (DynComp.comparability_file != null) {
+        if (Premain.verbose) {
+          System.out.println("Writing comparability sets to " + DynComp.comparability_file);
+        }
+        PrintWriter compare_out = open(DynComp.comparability_file);
+        long startTime = System.nanoTime();
+        DCRuntime.printAllComparable(compare_out);
+        compare_out.close();
+        if (Premain.verbose) {
+          long duration = System.nanoTime() - startTime;
+          System.out.printf(
+              "Comparability sets written in %ds%n", TimeUnit.NANOSECONDS.toSeconds(duration));
+        }
+      }
+
+      if (DynComp.trace_file != null) {
+        if (Premain.verbose) {
+          System.out.println("Writing traced comparability sets to " + DynComp.trace_file);
+        }
+        PrintWriter trace_out = open(DynComp.trace_file);
+        long startTime = System.nanoTime();
+        DCRuntime.traceAllComparable(trace_out);
+        trace_out.close();
+        if (Premain.verbose) {
+          long duration = System.nanoTime() - startTime;
+          System.out.printf(
+              "Traced comparability sets written in %ds%n",
+              TimeUnit.NANOSECONDS.toSeconds(duration));
+        }
+      } else {
+        // Writing comparability sets to standard output?
+      }
+
+      if (Premain.verbose) {
+        DCRuntime.decl_stats();
+      }
+
+      // Write the decl file out
+      @SuppressWarnings("nullness:argument") // DynComp guarantees decl_file is non null
+      File decl_file = new File(DynComp.output_dir, DynComp.decl_file);
+      if (Premain.verbose) System.out.println("Writing decl file to " + decl_file);
+      PrintWriter decl_fp = open(decl_file);
+      // Create DeclWriter so can share output code in Chicory.
+      DCRuntime.declWriter = new DeclWriter(decl_fp);
+      DCRuntime.declWriter.debug = DynComp.debug_decl_print;
+      // Used for calling ComparabilityProvider.getComparability.
+      DCRuntime.comparabilityProvider = new DCRuntime();
+
+      long startTime = System.nanoTime();
+      DCRuntime.printDeclFile(decl_fp);
+      decl_fp.close();
+      if (Premain.verbose) {
+        long duration = System.nanoTime() - startTime;
+        System.out.printf("Decl file written in %ds%n", TimeUnit.NANOSECONDS.toSeconds(duration));
+        System.out.printf("comp_list = %,d%n", DCRuntime.comp_list_ms);
+        System.out.printf("ppt name  = %,d%n", DCRuntime.ppt_name_ms);
+        System.out.printf("decl vars = %,d%n", DCRuntime.decl_vars_ms);
+        System.out.printf("total     = %,d%n", DCRuntime.total_ms);
+      }
+      if (Premain.verbose) System.out.println("DynComp complete");
+    }
+  }
+
+  /**
+   * Helper method to create a PrintWriter from a File.
+   *
+   * @param filename the File to be opened
+   * @return a new PrintWriter from filename
+   */
+  public static PrintWriter open(File filename) {
+    File canonicalFile;
+    try {
+      canonicalFile = filename.getCanonicalFile();
+    } catch (IOException e) {
+      throw new Error(
+          "Can't get canonical file for " + filename + " in " + System.getProperty("user.dir"));
+    }
+
+    // I don't know why, but without this, the call to newBufferedWriter fails in some contexts.
+    try {
+      canonicalFile.createNewFile();
+    } catch (IOException e) {
+      throw new Error("createNewFile failed for " + canonicalFile, e);
+    }
+
+    try {
+      return new PrintWriter(Files.newBufferedWriter(canonicalFile.toPath(), UTF_8));
+    } catch (Exception e) {
+      throw new Error("Can't open " + filename + " = " + canonicalFile, e);
+    }
+  }
+}
