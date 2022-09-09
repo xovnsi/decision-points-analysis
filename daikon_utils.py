30efe5ba072fe6b2a076dc7f9a6dcf7df7ba9db8
Diego Savoia <diego.savoia@mail.polimi.it>
2022-09-09 17:01:54 +0200

    Changed os.replace to shutil.move

--- daikon_utils.py
+++ daikon_utils.py
@@ -1,5 +1,6 @@
 import os
 import math
+import shutil
 import subprocess
 from typing import Union
 from operator import itemgetter
@@ -81,7 +82,7 @@ def _get_daikon_invariants(dataset) -> list:
     """
 
     dataset.drop(columns=['target']).to_csv(path_or_buf='dataset.csv', index=False)
-    os.replace('/app/pm4py-test/checkargs.pm', '/etc/perl/checkargs.pm')
+    shutil.move('/app/pm4py-test/checkargs.pm', '/etc/perl/checkargs.pm')
     subprocess.run(['perl', 'daikon-5.8.10/scripts/convertcsv.pl', 'dataset.csv'])
     subprocess.run(['java', '-cp', 'daikon-5.8.10/daikon.jar', 'daikon.Daikon', '--nohierarchy', '-o', 'invariants.inv',
                     '--no_text_output', '--noversion', '--omit_from_output', 'r', 'dataset.dtrace', 'dataset.decls'])
