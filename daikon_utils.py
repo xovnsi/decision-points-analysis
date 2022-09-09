e3c92e0ff762bce5cd74b2b8c0d41c60fcc58c1e
Diego Savoia <diego.savoia@mail.polimi.it>
2022-09-09 17:48:00 +0200

    Testing again checkargs installation

--- daikon_utils.py
+++ daikon_utils.py
@@ -82,8 +82,8 @@ def _get_daikon_invariants(dataset) -> list:
     """
 
     dataset.drop(columns=['target']).to_csv(path_or_buf='dataset.csv', index=False)
-    subprocess.run(['perl', '-MCPAN', '-e', 'shell'])
-    subprocess.run(['install', 'HTML::CheckArgs'])
+    subprocess.run(['curl', '-L', 'http://cpanmin.us', '|', 'perl', '-', '--self-upgrade'])
+    subprocess.run(['cpanm', 'HTML::CheckArgs'])
     subprocess.run(['perl', 'daikon-5.8.10/scripts/convertcsv.pl', 'dataset.csv'])
     subprocess.run(['java', '-cp', 'daikon-5.8.10/daikon.jar', 'daikon.Daikon', '--nohierarchy', '-o', 'invariants.inv',
                     '--no_text_output', '--noversion', '--omit_from_output', 'r', 'dataset.dtrace', 'dataset.decls'])
