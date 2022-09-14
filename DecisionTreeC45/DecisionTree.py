3f8060e5d07bc23a66a1fe7be2b30a6dea3d72b6
Diego Savoia <diego.savoia@mail.polimi.it>
2022-09-14 14:55:16 +0200

    Upload updated DT algorithm version

--- DecisionTreeC45/DecisionTree.py
+++ DecisionTreeC45/DecisionTree.py
@@ -1,18 +1,22 @@
-from DecisionTreeC45.decision_tree_utils import get_split_gain, get_total_threshold, extract_rules_from_leaf
-from DecisionTreeC45.Nodes import DecisionNode, LeafNode
+from decision_tree_utils import class_entropy, get_split_gain, get_total_threshold, extract_rules_from_leaf
+from Nodes import DecisionNode, LeafNode
 from typing import Union
 import pandas as pd
 import numpy as np
+import scipy.stats as stats
+from operator import itemgetter
+from tqdm import tqdm
+import multiprocessing
 
 
 class DecisionTree(object):
     """ Implements a decision tree with C4.5 algorithm """
 
-    def __init__(self, attributes_map, max_depth=7):
+    def __init__(self, attributes_map, max_depth=20):
         self._nodes = set()
         self._root_node = None
         self.max_depth = max_depth
-        # attributes map is a disctionary contianing the type of each attribute in the logs.
+        # attributes map is a disctionary contianing the type of each attribute in the data.
         # Must be one of ['categorical', 'boolean', 'continuous']
         for attr_name in attributes_map.keys():
             if not attributes_map[attr_name] in ['continuous', 'categorical', 'boolean']:
@@ -39,7 +43,7 @@ class DecisionTree(object):
             raise Exception("Can't add node {}. Parent label not present in the tree".format(node._label))
 
     def _predict(self, row_in, node, predictions_dict) -> dict:
-        """ Recursively traverse the tree (given the logs in "row_in") until a leaf node and returns the class distribution """
+        """ Recursively traverse the tree (given the data in "row_in") until a leaf node and returns the class distribution """
         if node is None:
             raise Exception("Can't traverse the tree. Node is None")
         attribute = node.get_attribute().split(':')[0]
@@ -135,11 +139,11 @@ class DecisionTree(object):
         return out_func
 
     def get_split(self, data_in) -> Union[float, float, str]:
-        """ Compute the best split of the input logs """
+        """ Compute the best split of the input data """
         max_gain_ratio = None
         threshold_max_gain_ratio = None
         split_attribute = None
-        # if there is only the target column or the aren't logs the split doesn't exist
+        # if there is only the target column or the aren't data the split doesn't exist 
         if len(data_in['target'].unique()) > 1 and len(data_in) > 0:
             # in order the split to be chosen, its information gain must be at least equal to the mean of all the tests considered 
             tests_examined = {'gain_ratio': list(), 'info_gain': list(),
@@ -154,7 +158,7 @@ class DecisionTree(object):
                     tests_examined['threshold'].append(threshold)
                     tests_examined['attribute'].append(column)
                     tests_examined['not_near_trivial_subset'].append(are_there_at_least_two)
-            # breakpoint()
+            #breakpoint()
             # select the best split
             tests_examined = pd.DataFrame.from_dict(tests_examined)
             mean_info_gain = tests_examined['info_gain'].mean()
@@ -190,7 +194,7 @@ class DecisionTree(object):
             data_in = data_in.copy(deep=True) 
             data_in = data_in.drop(columns=[node.get_label().split()[0]])
         max_gain_ratio, local_threshold, split_attribute = self.get_split(data_in)
-        # breakpoint()
+        #breakpoint()
         # compute error predicting the most frequent class without splitting
         node_errors = data_in['target'].value_counts().sum() - data_in['target'].value_counts().max()
         # compute percentage
@@ -225,14 +229,14 @@ class DecisionTree(object):
                     # Low split
                     low_split_node = DecisionNode('{} <= {}'.format(split_attribute, float(threshold)), node.get_level())
                     self.add_node(low_split_node, node)
-                    # the split is computed on the known logs and then weighted on unknown ones
+                    # the split is computed on the known data and then weighted on unknown ones
                     data_known = data_in[data_in[split_attribute] != '?']
                     data_unknown = data_in[data_in[split_attribute] == '?']
                     weight_unknown = len(data_known[data_known[split_attribute] <= threshold]) / len(data_known)
                     new_weight = (np.array([weight_unknown] * len(data_unknown)) * np.array(data_unknown['weight'].copy(deep=True))).tolist()
                     new_data_unknown = data_unknown.copy(deep=True)
                     new_data_unknown.loc[:, ['weight']] = new_weight
-                    # concat the unknown logs to the known ones, weighted, and pass to the next split
+                    # concat the unknown data to the known ones, weighted, and pass to the next split
                     new_data_low = pd.concat([data_known[data_known[split_attribute] <= threshold], new_data_unknown], ignore_index=True)
                     self.split_node(low_split_node, new_data_low, data_total)
                     # High split
@@ -253,19 +257,19 @@ class DecisionTree(object):
                         # create DecisionNode, recursion and add node
                         child_node = DecisionNode('{} = {}'.format(split_attribute, attr_value), node.get_level())
                         self.add_node(child_node, node)
-                        # the split is computed on the known logs and then weighted on unknown ones
+                        # the split is computed on the known data and then weighted on unknown ones
                         weight_unknown = len(data_known[data_known[split_attribute] == attr_value]) / len(data_known)
                         new_weight = (np.array([weight_unknown] * len(data_unknown)) * np.array(data_unknown['weight'].copy(deep=True))).tolist()
                         new_data_unknown = data_unknown.copy(deep=True)
                         new_data_unknown.loc[:, ['weight']] = new_weight
-                        # concat the unknown logs to the known ones, weighted, and pass to the next split
+                        # concat the unknown data to the known ones, weighted, and pass to the next split
                         new_data = pd.concat([data_known[data_known[split_attribute] == attr_value], new_data_unknown], ignore_index=True)
                         self.split_node(child_node, new_data, data_total)
         else:
             # the node (default type "DecisionNode") is "transformed" in a leaf node ("LeafNode" type)
             parent_node = node.get_parent_node()
             if parent_node is None and node.get_label() == 'root':
-                print("The logs are not feasible for fitting a tree. Can't find a suitable split of the root node.")
+                print("The data are not feasible for fitting a tree. Can't find a suitable split of the root node.")
             elif parent_node is None:
                 raise Exception("Can't transform DecisionNode {} in LeafNode: no parent found".format(node.get_label()))
             else:
@@ -339,3 +343,178 @@ class DecisionTree(object):
             rules[target_class] = ' || '.join(rules[target_class])
 
         return rules
+
+    def extract_rules_with_pruning(self, data_in) -> dict:
+        """ Extracts the rules from the tree, one for each target transition.
+
+        For each leaf node, takes the list of conditions from the root to the leaf, simplifies it if possible, and puts
+        them in conjunction, adding the resulting rule to the dictionary at the corresponding target class key.
+        Finally, all the rules related to different leaves with the same target class are put in disjunction.
+        """
+
+        # Starting with a p_value threshold for the Fisher's Exact Test (for rule pruning) of 0.01, create the rules
+        # dictionary. If for some target all the rules have been pruned, repeat the process increasing the threshold.
+        p_threshold = 0.01
+        keep_rule = set()
+        while p_threshold <= 1.0:
+            rules = dict()
+
+            leaves = self.get_leaves_nodes()
+            inputs = [(leaf, keep_rule, p_threshold, data_in) for leaf in leaves]
+            print("Starting multiprocessing rules pruning on {} leaves...".format(str(len(leaves))))
+            with multiprocessing.Pool() as pool:
+                result = list(tqdm(pool.imap(self._simplify_rule_multiprocess, inputs), total=len(leaves)))
+
+            for (vertical_rules, leaf_class) in result:
+                # Create the set corresponding to the target class in the rules dictionary, if not already present
+                if leaf_class not in rules.keys():
+                    rules[leaf_class] = set()
+
+                # If the resulting list is composed by at least one rule, put them in conjunction and add the result to
+                # the dictionary of rules, at the corresponding class label
+                if len(vertical_rules) > 0:
+                    vertical_rules = " && ".join(vertical_rules)
+                    rules[leaf_class].add(vertical_rules)
+
+            # Put the rules for the same target class in disjunction. If there are no rules for some target class (they
+            # have been pruned) then set the 'empty_rule' variable to True.
+            empty_rule = False
+            for target_class in rules.keys():
+                if len(rules[target_class]) == 0:
+                    empty_rule = True
+                    break
+                else:
+                    rules[target_class] = " || ".join(rules[target_class])
+
+            # If 'empty_rule' is True, then increase the threshold and repeat the process. Otherwise, if two target
+            # transitions have the same rule (because the original vertical rule has been pruned "too much"), repeat the
+            # process but avoid simplifying the rule that originated the problem. This is done only if the 'new' rules
+            # to be avoided are not all already present in 'keep_rule', otherwise it means that the process is looping.
+            # If that happens, simply increase the threshold and repeat the process.
+            # Otherwise, return the dictionary.
+            if empty_rule:
+                # TODO maybe increase more each time? This is precise but it may take long since the cap is 1
+                keep_rule = set()
+                p_threshold = round(p_threshold + 0.01, 2)
+            elif len(rules.values()) != len(set(rules.values())):
+                rules_to_add = [r for r in set(rules.values()) if list(rules.values()).count(r) > 1]
+                if not all([r in keep_rule for r in rules_to_add]):
+                    keep_rule.update(rules_to_add)
+                else:
+                    keep_rule = set()
+                    p_threshold = round(p_threshold + 0.01, 2)
+            else:
+                break
+
+        return rules
+
+    def _simplify_rule_multiprocess(self, input):
+        leaf_node, kr, p_threshold, data_in = input
+        vertical_rules = extract_rules_from_leaf(leaf_node)
+
+        # Simplify the list of rules, if possible (and if vertical_rules does not contain rules in keep_rule)
+        if not any([r in vertical_rules for r in kr]):
+            vertical_rules = self._simplify_rule(vertical_rules, leaf_node._label_class, p_threshold, data_in)
+
+        return vertical_rules, leaf_node._label_class
+
+    def _simplify_rule(self, vertical_rules, leaf_class, p_threshold, data_in) -> list:
+        """ Simplifies the list of rules from the root to a leaf node.
+
+        Given the list of vertical rules for a leaf, i.e. the list of rules from the root to the leaf node,
+        drops the irrelevant rules recursively applying a Fisher's Exact Test and returns the remaining ones.
+        In principle, all the rules could be removed: in that case, the result would be an empty list.
+        Method taken from "Simplifying Decision Trees" by J.R. Quinlan (1986).
+        """
+
+        rules_candidates_remove = list()
+        # For every rule in vertical_rules, check if it could be removed from vertical_rules.
+        # This is true if the related p-value returned by the Fisher's Exact Test is higher than the threshold.
+        # Indeed, a rule is considered relevant for the classification only if the null hypothesis (i.e. the two
+        # variables - table rows and columns - are independent) can be rejected at the threshold*100% level or better.
+        for rule in vertical_rules:
+            other_rules = vertical_rules[:]
+            other_rules.remove(rule)
+            table = self._create_fisher_table(rule, other_rules, leaf_class, data_in)
+            (_, p_value) = stats.fisher_exact(table)
+            if p_value > p_threshold:
+                rules_candidates_remove.append((rule, p_value))
+
+        # Among the candidates rules, remove the one with the highest p-value (the most irrelevant)
+        if len(rules_candidates_remove) > 0:
+            rule_to_remove = max(rules_candidates_remove, key=itemgetter(1))[0]
+            vertical_rules.remove(rule_to_remove)
+            # Then, recurse the process on the remaining rules
+            self._simplify_rule(vertical_rules, leaf_class, p_threshold, data_in)
+
+        return vertical_rules
+
+    def _create_fisher_table(self, rule, other_rules, leaf_class, data_in) -> pd.DataFrame:
+        """ Creates a 2x2 table to be used for the Fisher's Exact Test.
+
+        Given a rule from the list of rules from the root to the leaf node, the other rules from that list, the leaf
+        class and the training set, creates a 2x2 table containing the number of training examples that satisfy the
+        other rules divided according to the satisfaction of the excluded rule and the belonging to target class.
+        Missing values are not taken into account.
+        """
+
+        # Create a query string with all the rules in "other_rules" in conjunction (if there are other rules)
+        # Get the examples in the training set that satisfy all the rules in other_rules in conjunction
+        if len(other_rules) > 0:
+            query_other = ""
+            for r in other_rules:
+                r_attr, r_comp, r_value = r.split(' ')
+                query_other += r_attr
+                if r_comp == '=':
+                    query_other += ' == '
+                else:
+                    query_other += ' ' + r_comp + ' '
+                if data_in.dtypes[r_attr] in ['float64', 'bool']:
+                    query_other += r_value
+                else:
+                    query_other += '"' + r_value + '"'
+                if r != other_rules[-1]:
+                    query_other += ' & '
+            examples_satisfy_other = data_in.query(query_other)
+        else:
+            examples_satisfy_other = data_in.copy()
+
+        # Create a query with the excluded rule
+        rule_attr, rule_comp, rule_value = rule.split(' ')
+        query_rule = rule_attr
+        if rule_comp == '=':
+            query_rule += ' == '
+        else:
+            query_rule += ' ' + rule_comp + ' '
+        if data_in.dtypes[rule_attr] in ['float64', 'bool']:
+            query_rule += rule_value
+        else:
+            query_rule += '"' + rule_value + '"'
+
+        # Get the examples in the training set that satisfy the excluded rule
+        examples_satisfy_other_and_rule = examples_satisfy_other.query(query_rule)
+
+        # Get the examples in the training set that satisfy the other_rules in conjunction but not the excluded rule
+        examples_satisfy_other_but_not_rule = examples_satisfy_other[
+            ~examples_satisfy_other.apply(tuple, 1).isin(examples_satisfy_other_and_rule.apply(tuple, 1))]
+
+        # Create the table which contains, for every target class and the satisfaction of the excluded rule,
+        # the corresponding number of examples in the training set
+        table = {k1: {k2: 0 for k2 in [leaf_class, 'not '+leaf_class]} for k1 in ['satisfies rule', 'does not satisfy rule']}
+
+        count_other_and_rule = examples_satisfy_other_and_rule.groupby('target').count().iloc[:, 0]
+        count_other_but_not_rule = examples_satisfy_other_but_not_rule.groupby('target').count().iloc[:, 0]
+
+        for idx, value in count_other_and_rule.items():
+            if idx == leaf_class:
+                table['satisfies rule'][leaf_class] = value
+            else:
+                table['satisfies rule']['not '+leaf_class] = value
+        for idx, value in count_other_but_not_rule.items():
+            if idx == leaf_class:
+                table['does not satisfy rule'][leaf_class] = value
+            else:
+                table['does not satisfy rule']['not '+leaf_class] = value
+
+        table_df = pd.DataFrame.from_dict(table, orient='index')
+        return table_df
--- DecisionTreeC45/decision_tree_utils.py
+++ DecisionTreeC45/decision_tree_utils.py
@@ -1,7 +1,6 @@
 import numpy as np
 from copy import copy
 
-
 def extract_rules_from_leaf(node):
     """ Extract all the rules in the path from the leaf to the root """
     rules = list()
@@ -11,7 +10,6 @@ def extract_rules_from_leaf(node):
         current_node = current_node.get_parent_node()
     return rules
 
-
 def get_total_threshold(data, local_threshold) -> float:
     """ Computes the threshold on the total dataset
 
@@ -20,19 +18,17 @@ def get_total_threshold(data, local_threshold) -> float:
     data = data[data != '?'].astype(float)
     return data[data.le(local_threshold)].max()
 
-
 def class_entropy(data) -> float:
     """ Returns the weighted entropy of a split """
     ops = data.groupby('target')['weight'].sum() / data['weight'].sum()
     return - np.sum(ops * np.log2(ops))
 
-
 def get_split_gain(data_in, attr_type) -> [float, float, float, bool]:
     """ Computes the information gain, the gain ratio, the local threshold and the meaningfulness of the split
 
-    Given a split and the type of the attribute considered, the infomation gain is computed on known logs
-    (i.e. not '?') considerind their weight (reflecting the presence of unknonw logs in previous splits).
-    The gain ratio is computed considering one more class if unknown logs are present.
+    Given a split and the type of the attribute considered, the infomation gain is computed on known data
+    (i.e. not '?') considerind their weight (reflecting the presence of unknonw data in previous splits).
+    The gain ratio is computed considering one more class if unknown data are present.
     For the split to be meaningful, it has to have at least two subsplits with more than one example each.
     """
     attr_name = [col for col in data_in.columns if col != 'target'][0]
@@ -43,7 +39,7 @@ def get_split_gain(data_in, attr_type) -> [float, float, float, bool]:
     # if categorical number of split = number of attributes
     if attr_type in ['categorical', 'boolean']:
         data_counts = data_in[attr_name].value_counts()
-        # deals with unknown logs
+        # deals with unknown data
         total_count = len(data_in)
         known_count = len(data_in[data_in[attr_name] != '?'])
         freq_known = known_count / total_count
@@ -53,7 +49,7 @@ def get_split_gain(data_in, attr_type) -> [float, float, float, bool]:
                 split_gain -= freq_attr * class_entropy(data_in[data_in[attr_name] == attr_value][['target', 'weight']])
                 split_info += - freq_attr * np.log2(freq_attr)
             else:
-                # one more class for the unknown logs
+                # one more class for the unknown data
                 split_info += -(1 - freq_known) * np.log2(1 - freq_known)
         gain_ratio = (freq_known * split_gain) / split_info
         info_gain = split_gain
@@ -62,7 +58,7 @@ def get_split_gain(data_in, attr_type) -> [float, float, float, bool]:
         are_there_at_least_two = len([True for len_subset in len_subsets if len_subset >= 2]) >= 2
     # if continuous number of split = two
     elif attr_type == 'continuous':
-        # deals wìth unknown logs
+        # deals wìth unknown data
         freq_known = len(data_in[data_in[attr_name] != '?']) / len(data_in)
         data_in = data_in[data_in[attr_name] != '?']
         # sorted and compute thresolds
@@ -77,7 +73,7 @@ def get_split_gain(data_in, attr_type) -> [float, float, float, bool]:
             class_entropy_high = class_entropy(data_in[data_in[attr_name] > threshold][['target', 'weight']])
             split_gain_threshold = split_gain - freq_attr * class_entropy_low - (1 - freq_attr) * class_entropy_high 
             split_info = - freq_attr * np.log2(freq_attr) - (1 - freq_attr) * np.log2(1 - freq_attr) 
-            # one more class for the unknown logs
+            # one more class for the unknown data
             if freq_known < 1.0:
                 split_info += - (1 - freq_known) * np.log2(1 - freq_known) 
             gain_ratio_temp = (freq_known * split_gain_threshold) / split_info
